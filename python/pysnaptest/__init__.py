423d5f4c9c6e65f9fa890bad626da2c06870f771
Hugo Filstroff <hugofilstroff@gmail.com>
2025-01-01 14:59:13 +0100

    Ruff noqa

--- python/pysnaptest/__init__.py
+++ python/pysnaptest/__init__.py
@@ -1,7 +1,8 @@
+# ruff: noqa: F401
 from .snapshot import (
     snapshot,
     assert_json_snapshot,
     assert_csv_snapshot,
     assert_snapshot,
     assert_dataframe_snapshot,
-)  # noqa: F401
+)
