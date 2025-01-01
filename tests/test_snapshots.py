864f52c1e209cb9c8e5e0a017f7fba0083817092
Hugo Filstroff <hugofilstroff@gmail.com>
2025-01-01 15:03:06 +0100

    Add tests

--- tests/test_snapshots.py
+++ tests/test_snapshots.py
@@ -1,5 +1,6 @@
 from pysnaptest import snapshot, assert_json_snapshot, assert_dataframe_snapshot
 import pandas as pd
+import polars as pl
 
 
 @snapshot
@@ -17,20 +18,30 @@ def test_snapshot_list_result() -> list[str]:
     return [1, 2, 4]
 
 
-def test_assert_json_snapshot() -> list[str]:
+def test_assert_json_snapshot():
     assert_json_snapshot({"assert_json_snapshot": "expected_result"})
 
 
-def test_assert_snapshot() -> list[str]:
+def test_assert_snapshot():
     assert_json_snapshot("expected_result")
 
 
-def test_assert_dataframe_snapshot() -> list[str]:
+def test_assert_pandas_dataframe_snapshot():
     df = pd.DataFrame({"name": ["foo", "bar"], "id": [1, 2]})
     assert_dataframe_snapshot(df, index=False)
 
+@snapshot
+def test_assert_polars_dataframe_snapshot() -> pl.DataFrame:
+    return pl.DataFrame(
+    {
+        "foo": [1, 2, 3, 4, 5],
+        "bar": [6, 7, 8, 9, 10],
+        "ham": ["a", "b", "c", "d", "e"],
+    }
+)
 
-def test_assert_snapshot_multiple() -> list[str]:
+
+def test_assert_snapshot_multiple():
     snapshot_name_prefix = "test_snapshots_test_assert_snapshot_multiple"
     assert_json_snapshot("expected_result_1", snapshot_name=f"{snapshot_name_prefix}_1")
     assert_json_snapshot("expected_result_2", snapshot_name=f"{snapshot_name_prefix}_2")
